e971e55a0d88155a45297bbb0fd25ee8add3b46e
WuHaoYuan <joe82714@gmail.com>
2019-10-24 15:57:37 +0800

    footer fix

--- src/views/layout/Footer.vue
+++ src/views/layout/Footer.vue
@@ -3,8 +3,12 @@
     class="text-white"
     style="background-color:black"
   >
-    <div class="d-flex justify-content-between align-items-center container py-1">
+    <div class="d-md-flex d-none justify-content-between align-items-center container py-1">
       <span>資料來源 : 行政院環境保護署</span><span>Copyright © 2019 HaoYuan.All rights reserved.</span>
+    </div>
+    <div class="d-md-none text-center py-2">
+      <div>資料來源 : 行政院環境保護署</div>
+      <div>Copyright © 2019 HaoYuan.All rights reserved.</div>
     </div>
   </div>
 </template>
